f17d57846dc00d90cda2ef1ca3f88289957bbd7f
aleksandr <ostroverkhov.aleksandr@oberon-alfa.ru>
2023-06-01 19:26:00 +0300

    interface added

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -6,21 +6,21 @@
           <q-avatar>
             <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
           </q-avatar>
-          Depth of market
+          Quasar App
         </q-toolbar-title>
         <q-space />
         <q-toggle
-            v-model="darkMode"
-            @click="$q.dark.toggle()"
-            checked-icon="dark_mode"
-            color="deep-purple-10"
-            unchecked-icon="light_mode"
-            size="xl"
-          />
+          v-model="darkMode"
+          @click="$q.dark.toggle()"
+          checked-icon="dark_mode"
+          color="accent"
+          unchecked-icon="light_mode"
+          size="xl"
+        />
       </q-toolbar>
     </q-header>
-    <q-page-container>
-      <router-view />
+    <q-page-container :class="darkMode == false ? 'bg-info' : 'bg-dark-page'">
+      <router-view :darkMode="darkMode" />
     </q-page-container>
   </q-layout>
 </template>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,40 +1,116 @@
 <template>
-  <q-page>
-    <div class="q-pa-md">
-      <q-table
-        dense
-        title="Buy Order"
-        :rows="rowsBids"
-        :columns="columnsBids"
-        row-key="name"
-        :rows-per-page-options="[0]"
-        hide-bottom
-      >
-        <template v-slot:body-cell="props">
-          <q-td :props="props">
-            <q-badge :color="(props.row.fromUserOrder == true) ? 'warning' : 'positive'" :label="props.value" />
-          </q-td>
-        </template>
-      </q-table>
-    </div>
-    <div class="q-pa-md">
-      <q-table
-        dense
-        title="Sell Order"
-        :rows="rowsAsks"
-        :columns="columnsAsks"
-        row-key="name"
-        :rows-per-page-options="[0]"
-        hide-bottom
-      >
-        <template v-slot:body-cell="props">
-          <q-td :props="props">
-            <q-badge :color="(props.row.fromUserOrder == true) ? 'warning' : 'negative'" :label="props.value" />
-          </q-td>
-        </template>
-      </q-table>
-    </div>
-    <p class="text-h3">{{ spreadValue }}</p>
+  <q-page class="q-pa-sm">
+    <q-card class="q-mt-sm" bordered :class="darkMode == false ? 'bg-warning' : 'bg-dark'">
+      <q-card-section class="text-h6 q-pb-none">
+        <q-item>
+          <q-item-section avatar>
+            <q-icon color="secondary" name="analytics" size="44px"/>
+          </q-item-section>
+          <q-item-section>
+            <span class="text-h5">Order Book -
+              <span class="text-h6 text-secondary">BTC/USDT</span>
+            </span>
+          </q-item-section>
+        </q-item>
+      </q-card-section>
+      <q-card-section class="row" :class="darkMode == false ? 'bg-warning' : 'bg-primary'">
+        <div class="col-lg-7 col-sm-12 col-xs-12 col-md-7">
+          <div class="row">
+            <div class="col-lg-3 col-md-3 col-xs-6 col-sm-6">
+              <q-item>
+                <q-item-section top avatar>
+                  <q-avatar color="green-13" text-color="dark" icon="arrow_upward"/>
+                </q-item-section>
+                <q-item-section>
+                  <q-item-label class="text-body1 text-weight-medium">{{ bestBidPrice }} USDT</q-item-label>
+                  <q-item-label class="text-body1 text-weight-medium">{{ bestBidAmount }} BTC</q-item-label>
+                  <q-item-label caption>Best bid</q-item-label>
+                </q-item-section>
+              </q-item>
+            </div>
+            <div class="col-lg-3 col-md-3 col-xs-6 col-sm-6">
+              <q-item>
+                <q-item-section top avatar>
+                  <q-avatar color="red-12" text-color="dark" icon="arrow_downward"/>
+                </q-item-section>
+                <q-item-section>
+                  <q-item-label class="text-body1 text-weight-medium">{{ bestAskPrice }} USDT</q-item-label>
+                  <q-item-label class="text-body1 text-weight-medium">{{ bestAskAmount }} BTC</q-item-label>
+                  <q-item-label caption>Best ask</q-item-label>
+                </q-item-section>
+              </q-item>
+            </div>
+            <div class="col-lg-3 col-md-3 col-xs-6 col-sm-6">
+              <q-item>
+                <q-item-section top avatar>
+                  <q-avatar color="positive" text-color="dark" icon="percent"/>
+                </q-item-section>
+                <q-item-section>
+                  <q-item-label class="text-body1 text-weight-medium">{{ spreadValue }}</q-item-label>
+                  <q-item-label caption>Spread</q-item-label>
+                </q-item-section>
+              </q-item>
+            </div>
+            <div class="col-lg-3 col-md-3 col-xs-6 col-sm-6">
+              <q-item>
+                <q-item-section top avatar>
+                  <q-avatar color="yellow-14" text-color="dark" icon="sell"/>
+                </q-item-section>
+                <q-item-section>
+                  <q-item-label class="text-body1 text-weight-medium">User's orders</q-item-label>
+                </q-item-section>
+              </q-item>
+            </div>
+          </div>
+        </div>
+      </q-card-section>
+      <q-card-section>
+        <div class="row justify-around">
+          <div class="col-lg-4 col-md-5 col-xs-12 col-sm-6">
+            <div class="q-pa-md">
+              <q-table
+                bordered
+                :card-class="darkMode == false ? 'bg-info' : 'bg-primary'"
+                dense
+                title="Buy Order"
+                :rows="rowsBids"
+                :columns="columnsBids"
+                row-key="name"
+                :rows-per-page-options="[0]"
+                hide-bottom
+              >
+                <template v-slot:body-cell="props">
+                  <q-td :props="props">
+                    <q-badge class="text-body2 text-weight-medium text-dark" :color="(props.row.fromUserOrder == true) ? 'yellow-14' : 'green-13'" :label="props.value" />
+                  </q-td>
+                </template>
+              </q-table>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-5 col-xs-12 col-sm-6">
+            <div class="q-pa-md">
+              <q-table
+                bordered
+                :card-class="darkMode == false ? 'bg-info' : 'bg-primary'"
+                dense
+                title="Sell Order"
+                :rows="rowsAsks"
+                :columns="columnsAsks"
+                row-key="name"
+                :rows-per-page-options="[0]"
+                hide-bottom
+              >
+                <template v-slot:body-cell="props">
+                  <q-td :props="props">
+                    <q-badge class="text-body2 text-weight-medium text-dark" :color="(props.row.fromUserOrder == true) ? 'yellow-14' : 'red-12'" :label="props.value" />
+                  </q-td>
+                </template>
+              </q-table>
+            </div>
+          </div>
+        </div>
+      </q-card-section>
+    </q-card>
   </q-page>
 </template>
 
@@ -44,6 +120,9 @@ import { useOrdersStore } from '../stores/orders'
 
 export default defineComponent({
   name: 'IndexPage',
+  props: {
+    darkMode: Boolean
+  },
   setup () {
     const ordersStore = useOrdersStore()
     return { ordersStore }
@@ -51,6 +130,12 @@ export default defineComponent({
   data () {
     return {
       spreadValue: 0,
+
+      bestBidPrice: 0,
+      bestBidAmount: 0,
+
+      bestAskPrice: 0,
+      bestAskAmount: 0,
       columnsBids: [
         {
           name: 'bids',
@@ -140,9 +225,14 @@ export default defineComponent({
 
         const bids = orders('bids')
         const asks = orders('asks', 1)
-        const bestBid = [bids].sort(sortMaxBid)[0][0]
-        const bestAsk = [asks].sort(sortMaxAsk)[0][0]
-        this.spreadValue = +(bestAsk[0] - bestBid[0]).toFixed(2)
+
+        this.bestBidPrice = [bids].sort(sortMaxBid)[0][0][0].toFixed(2)
+        this.bestBidAmount = [bids].sort(sortMaxAsk)[0][0][1].toFixed(2)
+
+        this.bestAskPrice = [asks].sort(sortMaxBid)[0][0][0].toFixed(2)
+        this.bestAskAmount = [asks].sort(sortMaxAsk)[0][0][1].toFixed(2)
+
+        this.spreadValue = +(this.bestAskPrice - this.bestBidPrice).toFixed(2)
 
         function sortMaxBid (a, b) {
           if (a[0] === b[0] && a[1] === b[1]) {
